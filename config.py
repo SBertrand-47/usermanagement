fad75feebc73391c555c04a631371bc041df9518
Bertrand47 <sibomanabertrand@gmail.com>
2023-05-17 13:58:19 -0500

    Modified the config file

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-hard-to-guess-string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Umwana11@localhost/usermgt'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = database_url or os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False if os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') == 'False' else True
